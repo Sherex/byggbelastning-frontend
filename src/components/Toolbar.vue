24708abd33e340ee142895698d36b79e3aa7e865
Ingar Helgesen <ingar@i-h.no>
2020-08-20 14:08:37 +0200

    Added clickable logo & search field

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -6,16 +6,24 @@
   >
     <v-app-bar-nav-icon @click.stop="toggleSideDrawer()"></v-app-bar-nav-icon>
 
-    <v-btn icon x-large color="secondary" class="hidden-xs-only mr-3">
-      <v-img alt="logo" src="/apple-touch-icon.png" max-height="40" max-width="40">
-      </v-img>
-    </v-btn>
+    <router-link to="/">
+      <v-img class="toolbar-logo" alt="logo" src="/apple-touch-icon.png" max-height="40" max-width="40"/>
+    </router-link>
 
     <router-link to="/">
       <v-toolbar-title>Byggbelastning</v-toolbar-title>
     </router-link>
 
-    <div class="flex-grow-1"></div>
+    <v-spacer></v-spacer>
+    <v-text-field
+      solo-inverted
+      flat
+      hide-details
+      label="Search"
+      prepend-inner-icon="mdi-magnify"
+    ></v-text-field>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
   </v-app-bar>
 </template>
 
@@ -34,5 +42,8 @@ export default {
 a {
   text-decoration: none;
   color: unset !important;
+}
+.toolbar-logo {
+  margin: 0 15px 0 5px;
 }
 </style>
